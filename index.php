682c394f7b3b2868d7c74c286c8a7987ea9ac85f
BrunoZSgrott <31224957+BrunoZSgrott@users.noreply.github.com>
2021-08-09 22:43:32 -0300

    operation manager

--- index.php
+++ index.php
@@ -1,17 +1,35 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-        // put your code here
-        ?>
-    </body>
-</html>
+<?php
+
+define('ROOT_PATH', dirname(__FILE__));
+
+function catchException(Exception $ex) {
+    var_dump($ex->getTrace());
+}
+
+set_error_handler('catchException', 0);
+
+
+var_dump($_GET);
+$sData = $_GET['data'];
+//die();
+//$operation = $_GET['op'];
+
+$oManager = false;
+switch ($sData) {
+    case 'user':
+        require_once ROOT_PATH . '/Model/Manager/UserManager.inc';
+        $oManager = new \Model\Manager\UserManager();
+        break;
+    case 'info':
+        phpinfo();
+        break;
+}
+
+
+if ($oManager) {
+    switch ($operation) {
+        case 'create':
+            $oManager->create();
+            break;
+    }
+}
