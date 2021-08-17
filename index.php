b28ff79522ccdba63a9ba274cb0e92ee91290411
BrunoZSgrott <31224957+BrunoZSgrott@users.noreply.github.com>
2021-08-17 18:59:24 -0300

    desenvolvimento da solicitacao

--- index.php
+++ index.php
@@ -3,11 +3,11 @@
 define('ROOT_PATH', dirname(__FILE__));
 define('DEBUG', true);
 
-function catchException(Exception $ex) {
-    var_dump($ex->getTrace());
-}
-
-set_error_handler('catchException', 0);
+//function catchException(Exception $ex) {
+//    var_dump($ex->getTrace());
+//}
+//
+//set_error_handler('catchException', 0);
 
 
 $sData = $_GET['data'];
@@ -19,6 +19,10 @@ switch ($sData) {
         require_once ROOT_PATH . '/Model/Manager/UserManager.inc';
         $oManager = new \Model\Manager\UserManager();
         break;
+    case 'solicitacao':
+        require_once ROOT_PATH . '/Model/Manager/SolicitacaoManager.inc';
+        $oManager = new \Model\Manager\SolicitacaoManager();
+        break;
     case 'info':
         phpinfo();
         break;
@@ -28,10 +32,17 @@ switch ($sData) {
 if ($oManager) {
     switch ($operation) {
         case 'create':
-            $oManager->create();
+            $xResult = $oManager->create();
             break;
         case 'searchAll':
-            $oManager->searchAll();
+            $aDados = $oManager->searchAll();
+            $aData = [];
+            foreach ($aDados as $oModel) {
+                $aData[] = ($oModel->toJson());
+            }
+            $xResult = ($aData);
             break;
     }
 }
+
+echo json_encode($xResult);
--- userTestCase.php
+++ userTestCase.php
@@ -2,16 +2,18 @@
 
 $_GET = [];
 $_GET['data'] = 'user';
-$_GET['op'] = 'searchAll';
+$_GET['op'] = 'create';
 
 $_POST = [];
-//$_POST['codigo']= null;
-//$_POST['nome'] = 'Pedro Alvares';
-//$_POST['dataCadastro']= '01/10/2010';
-//$_POST['dataNascimento'] = '01/01/2000';
-//$_POST['documento'] = '55555555';
-//$_POST['telefone'] = '47 992924596';
-//$_POST['email'] = 'teste@mail.com';
+$_POST['codigo']= null;
+$_POST['nome'] = 'Pedro Alvares';
+$_POST['dataCadastro']= '01/10/2010';
+$_POST['dataNascimento'] = '01/01/2000';
+$_POST['documento'] = '55555555';
+$_POST['telefone'] = '47 992924596';
+$_POST['email'] = 'teste@mail.com';
 
 
+include './index.php';
+$_GET['op'] = 'searchAll';
 include './index.php';
